52c0bcd9842551712627f01d987ec288df01d2eb
Zack Olson <zacharykennetholson@gmail.com>
2015-09-02 15:53:22 -0700

    add images for about and projects, add project descriptions

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -6,22 +6,33 @@ Zack.Routers.Router = Backbone.Router.extend({
   routes: {
     '': 'root',
     'projects': 'projectsTab',
-    'about': 'aboutTab'
+    'about': 'aboutTab',
+    'pokemon': 'pokemonIndex'
   },
 
   root: function() {
-    var view = new Zack.Views.HomeView()
+    var view = new Zack.Views.HomeView();
+    $('#nav-tabs li').removeClass('active');
+    $('#nav-tabs li#home').addClass('active');
     this._swapView(view);
   },
 
   aboutTab: function() {
-    var view = new Zack.Views.AboutView()
+    var view = new Zack.Views.AboutView();
+    $('#nav-tabs li').removeClass('active');
+    $('#nav-tabs li#about').addClass('active');
     this._swapView(view);
   },
 
   projectsTab: function() {
     var view = new Zack.Views.ProjectsView()
+    $('#nav-tabs li').removeClass('active');
+    $('#nav-tabs li#projects').addClass('active');
     this._swapView(view);
+  },
+
+  pokemonIndex: function() {
+    
   },
 
   _swapView: function(view) {
