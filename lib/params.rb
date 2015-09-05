2c6e0d2b3e6696562916ccb1a7713d43ac4c6b26
Zack Olson <zacharykennetholson@gmail.com>
2015-09-05 15:00:53 -0700

    uses iterative params parsing, fixes scrolling lock on types show

--- lib/params.rb
+++ lib/params.rb
@@ -2,30 +2,29 @@ require 'uri'
 
 class Params
   def initialize(req, route_params = {})
-    ## set the params to a new hash with a default proc that initializes 
-    ## a deeper hash with the parent default proc.  this allows the params 
-    ## to be nested arbitrarily deep in parse_www_encoded_form
-    @params = Hash.new { |h, k| h[k] = Hash.new(&h.default_proc) }
-    @params.merge!(route_params)
+    @params = route_params
     parse_www_encoded_form(req.query_string) unless req.query_string.nil?
     parse_www_encoded_form(req.body) unless req.body.nil?
   end
 
   def [](key)
-    
-    ## check if the value is empty rather than truthy since the default proc 
-    ## will never return nil
-    @params[key.to_s] == {} ? @params[key.to_sym] : @params[key.to_s]
+    @params[key.to_s] || @params[key.to_sym]
   end
 
   class AttributeNotFoundError < ArgumentError; end;
 
   private  
   def parse_www_encoded_form(www_encoded_form)
-
-    ## this could and should be done iteratively (or recursively)
-    URI::decode_www_form(www_encoded_form).each do |(key, val)|
-      eval("@params#{parse_key(key).map { |k| "['#{k}']" }.join} = '#{val}'")
+    params_arr = URI::decode_www_form(www_encoded_form)
+    params_arr.each do |(keys, val)|     
+      current = @params
+      parsed = parse_key(keys)
+      parsed[0..-2].each do |key|
+        current[key] ||= {}
+        current = current[key] 
+      end
+      current[parsed.last] = val
+      @params.merge!(current)
     end
   end
 
